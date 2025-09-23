d606d891dc0fd2094c188c1358e5029b23c4fd16
MD_Therapies <deshpande.manjiree@gmail.com>
2025-09-23 09:47:33 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -211,7 +211,7 @@ function sendEmailNotification(name, email, therapy, message) {
             name: name,
             email: email,
             therapy: therapy,
-            message: message,
+            message: "NEW CONTACT FORM MESSAGE:\n\nName: " + name + "\nEmail: " + email + "\nTherapy Interest: " + therapy + "\nMessage: " + message + "\n\nReply directly to: " + email,
             reply_to: email
         }).then(function (response) {
             console.log('Notification email sent successfully!', response.status, response.text);
